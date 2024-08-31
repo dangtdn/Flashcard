d5233bf4359807c9f8cd423421249587ed0c0b40
dangtdn <nguyendanglop93@gmail.com>
2024-08-31 20:58:22 +0700

    update UI

--- src/components/common/Flashcard.jsx
+++ src/components/common/Flashcard.jsx
@@ -9,30 +9,32 @@ import {
   CarouselPrevious,
 } from "../ui/carousel";
 
-function Flashcard({ data, hidden }) {
+function Flashcard({ data }) {
   return (
     <Carousel className="w-full max-w-xs">
       <CarouselContent>
         {data?.map((item, index) => (
           <CarouselItem key={index}>
             <div className="p-1">
-              <Card className="shadow-black">
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <img src={item.srcImg} alt="img-kanji" />
-                </CardContent>
-                <CardFooter
-                  className={`flex flex-col justify-center items-center ${
-                    hidden && "opacity-0"
-                  }`}
-                >
-                  <p className="text-2xl font-[700]">
-                    {item.chineseCharacters}
-                  </p>
-                  <p className="text-xl  font-[400]">
-                    {item.vietnamCharacters}
-                  </p>
-                </CardFooter>
-              </Card>
+              <div className="group perspective w-64 h-64 [perspective:1000px]">
+                <Card className="relative h-full w-full rounded-xl shadow-black transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
+                  <div className="absoblute inset-0 h-full">
+                    <img
+                      className="h-full w-full rounded-xl object-cover"
+                      src={item.srcImg}
+                      alt="img-kanji"
+                    />
+                  </div>
+                  <div className="flex flex-col items-center justify-center absolute inset-0 h-full w-full rounded-xl bg-white px-12 text-center text-slate-900 [transform:rotateY(180deg)] [backface-visibility:hidden]">
+                    <p className="text-2xl font-[700]">
+                      {item.chineseCharacters}
+                    </p>
+                    <p className="text-xl  font-[400]">
+                      {item.vietnamCharacters}
+                    </p>
+                  </div>
+                </Card>
+              </div>
             </div>
           </CarouselItem>
         ))}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
 import Flashcard from "../../components/common/Flashcard";
 import { kanji_N5 } from "../../assets/list-kanji-N5";
-import { Checkbox } from "../../components/ui/checkbox";
 import { Button } from "../../components/ui/button";
 
 const HomePage = () => {
   const [cards, setCards] = useState(kanji_N5);
-  const [hidden, setHidden] = useState(false);
 
   const shuffleArray = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
@@ -27,27 +25,10 @@ const HomePage = () => {
       <div className="p-6">
         <p className="text-3xl font-bold uppercase">Flashcard Kanji N5</p>
         <div className="h-full flex justify-center items-center p-6">
-          <Flashcard data={cards} hidden={hidden} />
+          <Flashcard data={cards} />
         </div>
         <div className="h-full flex justify-start items-center gap-3 p-6">
           <Button onClick={handleClickRandom}>Random</Button>
-          {/* <Button
-            onClick={() => {
-              setCards([...kanji_N5]);
-            }}
-            variant="outline"
-          >
-            Reset
-          </Button> */}
-          <div className="flex items-center space-x-2">
-            <Checkbox id="terms" onClick={() => setHidden(!hidden)} />
-            <label
-              htmlFor="terms"
-              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-            >
-              Ẩn phiên âm Hán Việt
-            </label>
-          </div>
         </div>
       </div>
     </main>
